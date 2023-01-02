55321e21b083f501871d972c93b6adf6916f1c7e
tourniquet <admyn3d@gmail.com>
2023-01-02 19:06:15 +0000

    added tkinter GUI

--- income-counter.py
+++ income-counter.py
@@ -1,9 +1,48 @@
-import time
+import tkinter as tk
 
-hourly_rate = float(input('Please input your hour rate: '))
-total = 0
+window = tk.Tk()
+window.title('Income Counter')
 
-while True:
-  total += hourly_rate / 60 / 60
-  print(round(total, 2))
-  time.sleep(1)
+label = tk.Label(
+  text=0,
+  fg='black',
+  bg='#34A2FE',
+  width=10,
+  height=10
+)
+label.pack()
+
+entry = tk.Entry(
+  bg='red',
+  fg='yellow',
+  width=5,
+)
+entry.pack()
+entry.insert(0, 0)
+
+
+total_amount = 0
+hourly_rate = 0
+def update_amount():
+  global total_amount, hourly_rate
+  hourly_rate = float(entry.get())
+
+  window.after(1000, update_amount)
+
+  if hourly_rate > 0:
+    total_amount += hourly_rate / 60 / 60
+    label['text'] = round(total_amount, 2)
+
+
+button = tk.Button(
+  text = 'Start',
+  width=25,
+  height=5,
+  foreground='red',
+  background='yellow',
+  command=update_amount
+).pack()
+
+
+window.after(1000, update_amount)
+window.mainloop()
